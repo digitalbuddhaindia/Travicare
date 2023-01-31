a30084e54ab9fb3f57f83093014a746fe9942d7c
jeevancandorcode <jeevan@candorcode.com>
2023-01-31 11:32:18 +0530

    latest code of travicare jan 31

---
+++ src/components/Unique.js
@@ -0,0 +1,71 @@
+import React,{useContext} from 'react';
+import '../components/Unique.css';
+import uniquebackground from '../assets/images/uniquebackground.svg';
+import logo1 from '../assets/images/logo1.svg';
+import logo2 from '../assets/images/logo2.svg';
+import logo3 from '../assets/images/logo3.svg';
+import logo4 from '../assets/images/logo4.svg';
+import logo5 from '../assets/images/logo5.svg';
+import { MobileContext } from '../App';
+
+
+function Unique () {
+  const screenSize = useContext(MobileContext);
+
+  return (
+    <div>
+      {screenSize <= 500 ?(
+        <div>
+        <div className="unique-mobile">
+        <div className="black-shade" />
+        <div className="image-mobile-unique">
+        <img src={uniquebackground} />
+        <p className="unique-title-mobile">THE UNIQUE LOCATION</p>
+        <p className="unique-caption-mobile">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+        </p>
+        <button className="view-btn-mobile">VIEW MORE</button>
+        </div>
+        </div>
+        <div className="luxury-title-mobile">The luxurious stay</div>
+        <div className="luxury-caption-mobile">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+        </div>
+        <div className="unique-logos-mobile">
+        <img src={logo1} />
+        <img src={logo2} />
+        <img src={logo3} />
+        <img src={logo4} />
+        <img src={logo5} />
+      </div>
+       
+        </div>
+      ):<div className="unique-component">
+      <div className="image-box">
+        <img src={uniquebackground} />
+        <div className="black-shade" />
+        <p className="unique-title">THE UNIQUE LOCATION</p>
+        <p className="unique-caption">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+        </p>
+        <button className="view-btn">VIEW MORE</button>
+      </div>
+      <div className="luxury-stay">
+        <div className="luxury-title">The luxurious stay</div>
+        <div className="luxury-caption">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+        </div>
+      </div>
+      <div className="unique-logos">
+        <img src={logo1} />
+        <img src={logo2} />
+        <img src={logo3} />
+        <img src={logo4} />
+        <img src={logo5} />
+      </div>
+    </div>}
+    </div>
+    
+  );
+}
+export default Unique;
